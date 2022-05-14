da63faac8156f6415f8c98a60663589fb9568233
EdelDenis <edeldenis12@gmail.com>
2022-05-14 10:10:15 +0000

    j

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ let array = [1,2,3,4,5,6,7,8,9,10];
 let randomNumber = array[Math.floor(Math.random()*array.length)];
 console.log(randomNumber);
 
-
+function checkQuestion () {
+    while(question == "" || isNaN(question) || question == null){
+    question = prompt("Угадайте число от 1 до 10");
+    }
+}
 
 function playGame (event) {
     event.preventDefault();
     let counter = 3;
-while(counter > 0){
+    while(counter > 0){
     let question = prompt("Угадайте число от 1 до 10");
-    function checkQuestion () {
-        while(question == "" || isNaN(question) || question == null){
-        question = prompt("Угадайте число от 1 до 10");
-        }
-    }
+    
     checkQuestion();
 
     if(question == randomNumber){
