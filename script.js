b13a0472eceacca0004ebdc1eb9ff95077e4da62
EdelDenis <edeldenis12@gmail.com>
2022-05-19 13:35:34 +0000

    d

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 "use strict"
 
-const input = document.querySelector(".input")
-const button = document.querySelector(".button");
-const text = document.querySelector(".text")
+
 
 
 let array = [1,2,3,4,5,6,7,8,9,10];
 let randomNumber = array[Math.floor(Math.random()*array.length)];
 console.log(randomNumber);
 
-function checkQuestion () {
-    while(question == "" || isNaN(question) || question == null){
-    question = prompt("Угадайте число от 1 до 10");
-    }
-}
+let text = document.querySelector(".text");
+const addForm = document.querySelector("form.add");
+const addInput = addForm.querySelector(".adding__input");
+const btn = addForm.querySelector(".button");
 
-function playGame (event) {
+
+
+
+addForm.addEventListener("submit",(event)=>{
     event.preventDefault();
     let counter = 3;
-    while(counter > 0){
-    let question = prompt("Угадайте число от 1 до 10");
     
-    checkQuestion();
-
-    if(question == randomNumber){
-        text.style.color = "Green";
-        text.textContent = "Вы выиграли! Осталось попыток: " + counter;
-        break; 
+    let answer = addInput.value;
+    
+    if(answer == "" || isNaN(answer)){
+        text.style.color = "orange";
+        text.textContent = "Введите число !"
     } else {
-        text.style.color = "Red";
-        text.textContent = "Вы не угадали! Осталось попыток: " + counter;
-        counter--;
-    } if (counter == 0) {
-        text.style.color = "Red";
-        text.textContent = "Вы не угадали! Осталось попыток: " + counter;
-        break;
+        if(randomNumber == answer){
+            text.style.color = "green";
+            text.textContent = "Вы выиграли! Осталось попыток " + counter;
+            event.target.reset();
+        } else{
+            text.style.color = "red";
+            counter--;
+            text.textContent = "Вы проиграли! Осталось попыток " + counter; 
+            event.target.reset();
+        }
     }
-    
-}
-}
+})
 
 
 
-button.addEventListener("click", playGame);
+
 
 
 
